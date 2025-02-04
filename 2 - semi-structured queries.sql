aecfaf2c232f9cbb55572b89e723cbc40830b676
AsanteLA <119529382+AsanteLA@users.noreply.github.com>
2025-02-04 13:08:07 -0700

    Update 2 - semi-structured queries.sql

--- 2 - semi-structured queries.sql
+++ 2 - semi-structured queries.sql
@@ -8,6 +8,26 @@ select
 from car_sales 
 limit 2;
 
+-- EXAMPLES
+select
+    SRC:promotions[0] as first_promotion
+from 
+    car_sales
+limit 3;
+
+select
+    SRC:salesperson as salesperson_info,
+    SRC:customer[0].phone as phone
+from 
+    car_sales;
+
+select
+    SRC:date as sale_date,
+    SRC:vehicle[0].make as make,
+    SRC:vehicle[0].model as model,
+    SRC:vehicle[0].extras[0] as extra
+from 
+    car_sales;
 
 -- Write a query to extract the dealership and date fields from the SRC column, along with 
 -- the name of the salesperson from the SALESPERSON_INFO column.
@@ -15,10 +35,10 @@ limit 2;
 -- Then modify your query to drive the year of the sale from the date column.
 
 select 
-    * 
+    SRC:dealership as DealerName,
+    year(SRC:date :: date) as SaleYear -- use double colons to cast to date
 from car_sales 
 limit 2;
-
 
 -- Write a query to count the number of promotions (PROMOTIONS) applied to 
 -- each sale. Include the dealership from the SRC column, and try it with
@@ -55,10 +75,29 @@ limit 2;
 -- Write a query to flatten the VEHICLE_INFO array and extract the make, 
 -- model, and price of each vehicle for every sale.
 
+--example
+select
+    sale_id,
+    SRC:dealership as dealership,
+    pr.VALUE as promotion
+from
+    car_sales,
+lateral flatten(input => SRC:promotions) pr;
+
+select
+    pr.*
+from
+    car_sales,
+lateral flatten(input => SRC:promotions) pr;
+
+--------------------------------------------
+
 select 
     * 
 from car_sales 
 limit 2;
+
+
 
 
 -- Using LATERAL FLATTEN, calculate the total revenue (price) from all 
